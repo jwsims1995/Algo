cd7944d1ee53b883d85291a0e8245f0dd34a37e9
jaewon <jwsims@naver.com>
2021-03-18 23:28:20 +0900

    0318

---
+++ algo/src/ps/백준/B2/BJ_B2_1193_분수찾기.java
@@ -0,0 +1,22 @@
+package ps.백준.B2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_B2_1193_분수찾기 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer tokens;
+	static int x, cnt;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		x = Integer.parseInt(input.readLine());
+		
+		
+	}
+
+}
+
+// 0,0 -> 0,1 -> 1,0 -> 2,0 -> 1,1 -> 0,2 
