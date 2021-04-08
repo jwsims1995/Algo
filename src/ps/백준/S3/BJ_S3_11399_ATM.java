89bcf036d09a5b3a6a436d8d18517c667126af1d
jaewon <jwsims@naver.com>
2021-04-09 02:35:53 +0900

    0409

---
+++ src/ps/백준/S3/BJ_S3_11399_ATM.java
@@ -0,0 +1,14 @@
+package ps.백준.S3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_S3_11399_ATM {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer tokens;
+	public static void main(String[] args) {
+		
+	}
+}
