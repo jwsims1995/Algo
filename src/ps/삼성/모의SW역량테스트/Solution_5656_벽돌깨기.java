be545771f1d91387c57836b73082773ccb8aead8
jwsims <jwsims@naver.com>
2021-04-15 00:03:49 +0900

    0415

--- src/ps/삼성/모의SW역량테스트/Solution_5656_벽돌깨기.java
+++ src/ps/삼성/모의SW역량테스트/Solution_5656_벽돌깨기.java
@@ -1,5 +1,15 @@
 package ps.삼성.모의SW역량테스트;
 
-public class Solution_5656_벽돌깨기 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Solution_5656_벽돌깨기 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+	static StringBuilder output = new StringBuilder();
+	
+	public static void main(String[] args) {
+		
+	}
 }
