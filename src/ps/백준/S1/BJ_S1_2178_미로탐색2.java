b2deadc7a48b2c0f3ea0427cd808e0fb444cb8ce
jaewon <jwsims@naver.com>
2021-07-02 03:21:58 +0900

    0702

--- src/ps/백준/S1/BJ_S1_2178_미로탐색2.java
+++ src/ps/백준/S1/BJ_S1_2178_미로탐색2.java
@@ -9,13 +9,24 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class BJ_S1_2178_미로탐색 {
+public class BJ_S1_2178_미로탐색2 {
+	static class Point {
+		int r, c;
+
+		public Point(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+
+	}
+
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer tokens;
-	static int N, M;
+	static int N, M, ans;
 	static int[][] map;
 	static boolean[][] visit;
-	static int[][] deltas = {{1,0},{-1,0},{0,1},{0,-1}};
+	static int[][] deltas = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
 	public static void main(String[] args) throws IOException {
 		input = new BufferedReader(new StringReader(src));
 		tokens = new StringTokenizer(input.readLine());
@@ -25,56 +36,40 @@ public class BJ_S1_2178_미로탐색 {
 		for (int r = 0; r < N; r++) {
 			String line = input.readLine();
 			for (int c = 0; c < M; c++) {
-				map[r][c] = line.charAt(c)-'0';
+				map[r][c] = line.charAt(c) - '0';
 			}
 		}
 		for (int[] row : map) {
 			System.out.println(Arrays.toString(row));
 		}
-		bfs(0,0);
-		System.out.println(map[N-1][M-1]);
+		bfs(0, 0);
+		System.out.println(map[N - 1][M - 1]);
 	}
-	private static void bfs(int a, int b) {
+
+	private static void bfs(int r, int c) {
 		Queue<Point> queue = new LinkedList<>();
-		visit = new boolean[N][M];
-		queue.offer(new Point(a,b));
-		visit[a][b] = true;
-		while(!queue.isEmpty()) {
+		queue.offer(new Point(r, c));
+		boolean[][] visited = new boolean[N][M];
+		while (!queue.isEmpty()) {
 			Point p = queue.poll();
-			visit[p.r][p.c] = true;
+			visited[p.r][p.c] = true;
 			for (int d = 0; d < deltas.length; d++) {
 				int nr = p.r + deltas[d][0];
 				int nc = p.c + deltas[d][1];
-				
-				if(isIn(nr,nc)&&!visit[nr][nc] && map[nr][nc] == 1) {
-					
-						queue.offer(new Point(nr,nc));
-						map[nr][nc] = map[p.r][p.c] + 1; 
-					
+				if (isIn(nr, nc) && !visited[nr][nc] && map[nr][nc] == 1) {
+					visited[nr][nc] = true;
+					queue.offer(new Point(nr, nc));
+					map[nr][nc] = map[p.r][p.c] + 1;
 				}
-			}			
-		}
-	}
-	private static boolean isIn(int nr, int nc) {
-		return 0<=nr && nr < N && 0<=nc && nc<M;
-	}
-	static String src = "4 6\r\n" + 
-			"110110\r\n" + 
-			"110110\r\n" + 
-			"111111\r\n" + 
-			"111101";
-	
-	static class Point {
-		int r;
-		int c;
-		
-		public Point(int r, int c) {
-			super();
-			this.r = r;
-			this.c = c;
-		}
-		
-	}
-	
-}
+			}
 
+		}
+	}
+
+	private static boolean isIn(int nr, int nc) {
+		return 0 <= nr && nr < N && 0 <= nc && nc < M;
+	}
+
+	static String src = "4 6\r\n" + "110110\r\n" + "110110\r\n" + "111111\r\n" + "111101";
+
+}
--- src/ps/백준/S2/BJ_S2_11724_연결요소의개수.java
+++ src/ps/백준/S2/BJ_S2_11724_연결요소의개수.java
@@ -14,36 +14,54 @@ public class BJ_S2_11724_연결요소의개수 {
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
 
-	static int N, M;
-	static int[][] graph;
-	static boolean[][] visited;
+	static int N, M, cnt;
+	static boolean[][] graph;
+	static boolean[] visited;
 	
 	public static void main(String[] args) throws IOException {
 		input = new BufferedReader(new StringReader(src));
 		tokens = new StringTokenizer(input.readLine());
 		N = Integer.parseInt(tokens.nextToken());
 		M = Integer.parseInt(tokens.nextToken());
-		graph = new int[N+1][N+1];
-		visited = new boolean[N+1][N+1];
+		graph = new boolean[N+1][N+1];
+		visited = new boolean[N+1];
+		for(int m=0; m<M; m++) {
+			tokens = new StringTokenizer(input.readLine());
+			int start = Integer.parseInt(tokens.nextToken());
+			int end = Integer.parseInt(tokens.nextToken());
+			graph[start][end] = true;
+			graph[end][start] = true;
+		}
 		
-		int[] test = {};
-		test = new int[]{0,1};
-        System.out.println(Arrays.toString(test));
-        
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(1, map.getOrDefault(1, 0)+1);
-
-        for (int m = 0; m < map.size(); m++) {
-			map.get(key)
+		for(int start=1; start<=N; start++) {
+			if(!visited[start]) {
+				dfs(start);
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+	private static void dfs(int start) {
+		if(visited[start]) {
+			return;
+		}
+		visited[start] = true;
+		for (int n = 1; n < graph.length; n++) {
+			if(graph[start][n]) {
+				dfs(n);
+			}
 		}
 	}
 
 
-	
-	static String src = "6 5\r\n" + 
-			"1 2\r\n" + 
-			"2 5\r\n" + 
-			"5 1\r\n" + 
-			"3 4\r\n" + 
-			"4 6";//2
+
+	static String src = "6 8\n" + 
+			"1 2\n" + 
+			"2 5\n" + 
+			"5 1\n" + 
+			"3 4\n" + 
+			"4 6\n" + 
+			"5 4\n" + 
+			"2 4\n" + 
+			"2 3";//2
 }
