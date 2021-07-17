7b0bcae92cd304c869ab501d5ce349337d78a03b
jaewon <jwsims@naver.com>
2021-07-18 01:35:20 +0900

    0718

--- src/ps/백준/G5/BJ_G5_14567_선수과목.java
+++ src/ps/백준/G5/BJ_G5_14567_선수과목.java
@@ -12,46 +12,39 @@ public class BJ_G5_14567_선수과목 {
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
 	static int N, M;
-	static boolean[][] visited;
 	static boolean[][] subject;
 	public static void main(String[] args) throws IOException {
 		input = new BufferedReader(new StringReader(src));
 		tokens = new StringTokenizer(input.readLine());
 		N = Integer.parseInt(tokens.nextToken()); // 과목수
 		M = Integer.parseInt(tokens.nextToken()); // 선수조건 수
-		subject = new boolean[N][N];
-		visited = new boolean[N][N];
+		subject = new boolean[N+1][N+1];
+		int[] dp = new int[N+1];
 		
-		for (int n = 0; n < M; n++) {
+		for (int i = 0; i < M; i++) {
 			tokens = new StringTokenizer(input.readLine());
 			int pre = Integer.parseInt(tokens.nextToken());
 			int now = Integer.parseInt(tokens.nextToken());
 			subject[pre][now] = true;
 		}
 		
+		for (int i = 1; i <= N; i++) {
+			dp[i] = 1;
+			
+			for (int j = 1; j < i; j++) {
+				if(subject[j][i]) dp[i] = Math.max(dp[i], dp[j]+1);
+			}
+		}
 		
 		for (int now = 1; now <= N; now++) {
-			dfs(now, 0, 0);
-			output.append(" ");
+			output.append(dp[now]).append(" ");
 		}
 		System.out.println(output);
 	}
-	
-	
 
-	private static void dfs(int now, int pre, int cnt) {
-		if(visited[pre][now]) return;
-		
-		for (int p = 1; p < N; p++) {
-			if(!visited[p][now] && subject[p][now]) {
-				dfs(p,0,cnt+1);
-			}
-		}
-	}
-
-
-
-	private static String src = "3 2\n" + 
-			"2 3\n" + 
-			"1 2"; //1 2 3
+	private static String src = "6 4\n" + 
+			"1 2\n" + 
+			"1 3\n" + 
+			"2 5\n" + 
+			"4 5"; //1 2 3
 }
--- src/ps/백준/G5/BJ_G5_14719_빗물.java
+++ src/ps/백준/G5/BJ_G5_14719_빗물.java
@@ -11,7 +11,7 @@ public class BJ_G5_14719_빗물 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
-	static int H, W;
+	static int H, W, total;
 	static int[] height;
 	public static void main(String[] args) throws IOException {
 		input = new BufferedReader(new StringReader(src));
@@ -24,19 +24,18 @@ public class BJ_G5_14719_빗물 {
 			height[w] = Integer.parseInt(tokens.nextToken());
 		}//입력완료
 		
-		int maxHeight = height[0];
-		int total = 0;
-		for (int w = 0; w < W; w++) {
-			if(height[w] > maxHeight) {
-				maxHeight=height[w];
-				continue;
+		for (int w = 1; w < W-1; w++) {
+			int left =0 ;
+			int right = 0;
+			for (int front = 0 ; front<w ; front++) {
+				left= Math.max(height[front], left);
 			}
-			
-			if(height[w] < maxHeight) {
-				total += maxHeight - height[w];
+			for(int back = w; back<W; back++) {
+				right = Math.max(height[back], right);
 			}
+			int diff = Math.min(right, left);
+			if(height[w] < right && height[w] < left) total+= diff - height[w]; 
 		}
-		
 		System.out.println(total);
 	}
 	
