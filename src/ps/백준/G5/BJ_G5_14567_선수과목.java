89f620f6765c5473ad78956bd86524cf07a7df19
jaewon <jwsims@naver.com>
2021-07-17 13:51:59 +0900

    0717

--- src/ps/백준/G5/BJ_G5_14567_선수과목.java
+++ src/ps/백준/G5/BJ_G5_14567_선수과목.java
@@ -11,37 +11,47 @@ public class BJ_G5_14567_선수과목 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
-	static int H, W;
-	static int[] height;
+	static int N, M;
+	static boolean[][] visited;
+	static boolean[][] subject;
 	public static void main(String[] args) throws IOException {
 		input = new BufferedReader(new StringReader(src));
 		tokens = new StringTokenizer(input.readLine());
-		H = Integer.parseInt(tokens.nextToken());
-		W = Integer.parseInt(tokens.nextToken());
-		height = new int[W];
-		tokens = new StringTokenizer(input.readLine());
-		for (int w = 0; w < W; w++) {
-			height[w] = Integer.parseInt(tokens.nextToken());
-		}//입력완료
+		N = Integer.parseInt(tokens.nextToken()); // 과목수
+		M = Integer.parseInt(tokens.nextToken()); // 선수조건 수
+		subject = new boolean[N][N];
+		visited = new boolean[N][N];
 		
-		int maxHeight = height[0];
-		int total = 0;
-		for (int w = 0; w < W; w++) {
-			if(height[w] > maxHeight) {
-				maxHeight=height[w];
-				continue;
-			}
-			
-			if(height[w] < maxHeight) {
-				total += maxHeight - height[w];
-			}
+		for (int n = 0; n < M; n++) {
+			tokens = new StringTokenizer(input.readLine());
+			int pre = Integer.parseInt(tokens.nextToken());
+			int now = Integer.parseInt(tokens.nextToken());
+			subject[pre][now] = true;
 		}
 		
-		System.out.println(total);
+		
+		for (int now = 1; now <= N; now++) {
+			dfs(now, 0, 0);
+			output.append(" ");
+		}
+		System.out.println(output);
 	}
 	
 	
 
-	private static String src = "4 8\n" + 
-			"3 1 2 3 4 1 1 2"; //5
+	private static void dfs(int now, int pre, int cnt) {
+		if(visited[pre][now]) return;
+		
+		for (int p = 1; p < N; p++) {
+			if(!visited[p][now] && subject[p][now]) {
+				dfs(p,0,cnt+1);
+			}
+		}
+	}
+
+
+
+	private static String src = "3 2\n" + 
+			"2 3\n" + 
+			"1 2"; //1 2 3
 }
