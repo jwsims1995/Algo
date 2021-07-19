a95a038b9ccfa6c01806f2411cac582819838c41
jaewon <jwsims@naver.com>
2021-07-19 23:41:21 +0900

    0719

--- src/ps/백준/S1/BJ_S1_14888_연산자끼워넣기.java
+++ src/ps/백준/S1/BJ_S1_14888_연산자끼워넣기.java
@@ -1,11 +1,7 @@
 package ps.백준.S1;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class BJ_S1_14888_연산자끼워넣기 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -16,6 +12,8 @@ public class BJ_S1_14888_연산자끼워넣기 {
 	static int[] op = new int[4]; // + - * /
 	static List<Character> list = new ArrayList<Character>();
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		input = new BufferedReader(new StringReader(src));
 		N = Integer.parseInt(input.readLine());
 		A = new int[N];
 		tokens = new StringTokenizer(input.readLine());
@@ -26,6 +24,14 @@ public class BJ_S1_14888_연산자끼워넣기 {
 		for (int i = 0; i < 4; i++) {
 			op[i] += Integer.parseInt(tokens.nextToken());
 		}
-		for (int i = 0; i < 4; i++) {
-			for (int o = 0; o < op[i]; o++) {
-				if(i == 0) list.add(('+')
+		//입력완료
+		System.out.println(Arrays.toString(A));
+		System.out.println(Arrays.toString(op));
+		//입력확인
+		permutation(N-1, new boolean[4])
+		
+	}
+	static String src = "3\n" + 
+			"3 4 5\n" + 
+			"1 0 1 0";
+}
