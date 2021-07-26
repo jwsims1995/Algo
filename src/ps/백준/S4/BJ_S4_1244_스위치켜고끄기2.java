168c01ea3f018da32f92e10a0f0763ed2d7f46c8
jaewon <jwsims@naver.com>
2021-07-26 23:06:55 +0900

    0726

---
+++ src/ps/백준/S4/BJ_S4_1244_스위치켜고끄기2.java
@@ -0,0 +1,17 @@
+package ps.백준.S4;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ_S4_1244_스위치켜고끄기2 {
+	static BufferedReader input =new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+	static int N;
+	static int[] button;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		N = Integer.parseInt(input.readLine());
+		button = new int[N];
+		tokens =new StringTokenizer(input.readLine());
+		
+	}
+}
