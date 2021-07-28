21d078f4392ea946d46b020699780791598cfc37
jaewon <jwsims@naver.com>
2021-07-28 23:48:26 +0900

    0728

--- src/ps/백준/G5/BJ_G5_17836_공주님을구해라.java
+++ src/ps/백준/G5/BJ_G5_17836_공주님을구해라.java
@@ -4,102 +4,99 @@ import java.io.*;
 import java.util.*;
 
 public class BJ_G5_17836_공주님을구해라 {
-	static class Point{
-		int r, c;
-		public Point(int r, int c) {
+	static class Point {
+		int r, c, item, time;
+
+		public Point(int r, int c, int item, int time) {
 			this.r = r;
 			this.c = c;
+			this.item = item;
+			this.time = time;
 		}
 	}
-	static int[][] deltas = {{0,1},{0,-1},{1,0},{-1,0}};
+
+	static int[][] deltas = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
 	static int N, M, T, cnt;
 	static int[][] map;
 	static Queue<Point> queue;
-	static boolean[][] visited;
-	static boolean hasGram, isFound;
+	static boolean[][][] visited;
+	static boolean isFound;
 	public static void main(String[] args) throws IOException {
 		input = new BufferedReader(new StringReader(src));
-		tokens =new StringTokenizer(input.readLine());
+		tokens = new StringTokenizer(input.readLine());
 		N = Integer.parseInt(tokens.nextToken());
 		M = Integer.parseInt(tokens.nextToken());
 		T = Integer.parseInt(tokens.nextToken());
 
 		map = new int[N][M];
-		
+
 		for (int r = 0; r < N; r++) {
 			tokens = new StringTokenizer(input.readLine());
 			for (int c = 0; c < M; c++) {
 				map[r][c] = Integer.parseInt(tokens.nextToken());
 			}
-		}//입력완료
+		} // 입력완료
 //		for(int[] row:map) {
 //			System.out.println(Arrays.toString(row));
 //		}
-		
-		bfs(0,0);
-		
-		System.out.println(--cnt<=T && isFound?cnt:"Fail");
-//		System.out.println(cnt);
 
+		bfs();
+
+		System.out.println(isFound && cnt <= T ? cnt : "Fail");
+//		System.out.println(cnt);
 	}
 
-	private static void bfs(int r, int c) {
+	private static void bfs() {
 		queue = new LinkedList<>();
-		visited = new boolean[N][M];
-		queue.offer(new Point(r,c));
-		visited[r][c] = true;
-		
-		while(!queue.isEmpty()) {
-			int size=  queue.size();
-			boolean tmp = false;
-			while(size-->0) {
-				Point p = queue.poll();
-				if(p.r==N-1 && p.c == M-1 ) {
-					isFound = true;
-					cnt++;
-					return;
-				}
-				
-				int dlen = deltas.length;
-				for (int d = 0; d < dlen; d++) {
-					int nr = p.r + deltas[d][0];
-					int nc = p.c + deltas[d][1];
-					
+		visited = new boolean[N][M][2];
+		queue.offer(new Point(0, 0, 0, 0));
+		visited[0][0][0] = true;
+		while (!queue.isEmpty()) {
+			Point p = queue.poll();
+			if (p.r == N - 1 && p.c == M - 1) {
+				cnt = p.time;
+				isFound = true;
+				return;
+			}
 
-					
-					
-					if(isIn(nr,nc) && !hasGram &&!visited[nr][nc] && map[nr][nc] != 1) { // 그람이 없을때
-						queue.offer(new Point(nr,nc));
-						visited[nr][nc] =true;
-						
-						if(map[nr][nc] == 2) {//그람위치부터 시작
-							queue.offer(new Point(nr,nc));
-							tmp = true;
+			int dlen = deltas.length;
+			for (int d = 0; d < dlen; d++) {
+				int nr = p.r + deltas[d][0];
+				int nc = p.c + deltas[d][1];
+				int nt = p.time + 1;
+				int ni = p.item;
+				if (ni == 0) {// 그람이 없을때
+					if (isIn(nr, nc) && !visited[nr][nc][ni] && map[nr][nc] != 1) { 
+						if (map[nr][nc] == 0) {
+							queue.offer(new Point(nr, nc, ni, nt));
+							visited[nr][nc][ni] = true;
+						}
+
+						else if (map[nr][nc] == 2) {
+							queue.offer(new Point(nr, nc, ni+1, nt));
+							visited[nr][nc][ni+1] = true;
 						}
 					}
-					
-					else if(isIn(nr,nc) && hasGram &&!visited[nr][nc]) { //그람을 얻었을때
-						queue.offer(new Point(nr,nc));
-						visited[nr][nc] =true;
+				} else { // 그람을 얻었을때
+					if (isIn(nr, nc) && !visited[nr][nc][ni]) {
+						queue.offer(new Point(nr, nc, ni, nt));
+						visited[nr][nc][ni] = true;
 					}
 				}
+
 			}
-			if(tmp) hasGram = true;
-			cnt++;
 		}
-		
 	}
 
 	private static boolean isIn(int nr, int nc) {
-		return nr>=0 && nr<N && nc>=0 && nc<N ;
+		return nr >= 0 && nr < N && nc >= 0 && nc < M;
 	}
 
-	private static String src = "4 4 8\n" + 
-			"0 1 0 2\n" + 
-			"0 0 0 1\n" +
-			"1 1 1 0\n" +
-			"0 0 0 0";
+	private static String src = "3 4 6\n" + 
+			"0 1 0 1\n" + 
+			"0 0 0 1\n" + 
+			"0 0 1 0";
 }
--- src/ps/백준/S3/BJ_S3_2805_나무자르기.java
+++ src/ps/백준/S3/BJ_S3_2805_나무자르기.java
@@ -7,34 +7,45 @@ public class BJ_S3_2805_나무자르기 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
-	static int N, M, sum, ans;
+	static int N, M, ans;
 	static int[] num;
-	static PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
-		@Override
-		public int compare(Integer o1, Integer o2) {
-			return o2-o1;
-		}
-		
-	});
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		input= new BufferedReader(new StringReader(src));
 		tokens = new StringTokenizer(input.readLine());
 		N = Integer.parseInt(tokens.nextToken()); // N개 나무
 		M = Integer.parseInt(tokens.nextToken()); // M만큼 들고감
-		num = new int[N];
+		num = new int[N+1];
 		tokens =new StringTokenizer(input.readLine());
-		for(int n=0; n<N; n++) {
-			pq.add(Integer.parseInt(tokens.nextToken()));
+		int max = 0;
+		int min = 0;
+		for (int n = 1; n <= N; n++) {
+			num[n] = Integer.parseInt(tokens.nextToken());
+			max = Math.max(max, num[n]);
 		}
 		
-		while(sum<M) { 		
-			pq.offer(pq.poll() -1);
-			sum++;
-		}
 
 		
-		System.out.println(pq.poll());
+		while(min<=max) {
+			int mid = (max + min)/2;
+			long sum = 0;
+			
+			System.out.println("mid: "+mid);
+			for (int n = 1; n <= N; n++) {
+				if(num[n]>mid)
+					sum+= num[n]-mid; 
+			}
+			System.out.println(sum);
+			if(sum >= M) {
+				min = mid+1;
+				ans = mid;
+			}
+			else {
+				max = mid-1;
+			}
+			
+		}
+		System.out.println(ans);
 	}
 
 	static String src = "5 20\n" + 
