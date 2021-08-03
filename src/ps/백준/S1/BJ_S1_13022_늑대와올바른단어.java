d1b92822cf921e671c481b3be784b35826bcb82b
jaewon <jwsims@naver.com>
2021-08-03 23:01:18 +0900

    0803

--- src/ps/백준/S1/BJ_S1_13022_늑대와올바른단어.java
+++ src/ps/백준/S1/BJ_S1_13022_늑대와올바른단어.java
@@ -11,47 +11,67 @@ public class BJ_S1_13022_늑대와올바른단어 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer tokens;
 	
-	static char[] cycle = {'w', 'o', 'l', 'f'};
-	static int[] count = new int[4];
+	static char[] cycle = {'w','o','l','f'};
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		input = new BufferedReader(new StringReader(src));
+				
+		System.out.println(sol()?1:0);
+
 		
-		char[] words = input.readLine().toCharArray();
 		
-		System.out.println(sol(words));
-		
+	}
+
+	
+	static boolean sol() {
+		boolean isCorrect = true;
+		char[] spell = src.toCharArray();
+		int W = 0;
+		int O = 0;
+		int L = 0;
+		int F = 0;
+		int size = spell.length; //전체 길이
+		for (int i = 0; i < size; i++) {
+			if(i+1<size) {
+				if(spell[i] == 'f' && spell[i+1] == 'w') {
+					
+					if(W != O || O != L  || L != F+1) {
+						isCorrect = false;
+						return isCorrect;
+					}
+						W=0;
+						O=0;
+						L=0;
+						F=0;
+					}
+				
+				else if(spell[i] == 'w' && spell[i+1] == 'w' || spell[i] == 'w' && spell[i+1] == 'o') {
+					W++;
+				}
+			
+				else if(spell[i] == 'o' && spell[i+1] == 'o' || spell[i] == 'o' && spell[i+1] == 'l') {
+					O++;
+				}
+				else if(spell[i] == 'l' && spell[i+1] == 'l' || spell[i] == 'l' && spell[i+1] == 'f') {
+					L++;
+				}
+				else if(spell[i] == 'f' && spell[i+1] == 'f') {
+					F++;
+				}
+				else {
+					isCorrect = false;
+					return isCorrect;
+				}
+			}
+		}
+		if(W != O || O != L  || L != F+1) {
+			isCorrect = false;
+			return isCorrect;
+		}
+		return isCorrect;
 	}
 	
-	private static int sol(char[] words) {
-		int size = words.length;
-		int cnt = 0;
-		for (int i = 0; i < size; i++) {
-			if(words[0] != cycle[0]) return 0;
-			if(words[i] == cycle[cnt%4]) { // w->w, o->o. l->l, f->f
-				count[cnt%4]++;
-				continue;
-			}
-			
-			if(words[i] == cycle[(cnt+1)%4]) { //w->o, o->l, l->f, f-> w
-				cnt++;
-				count[cnt%4]++;
-				continue;
-			}
-			return 0;
-				
-		}
-		
-		System.out.println(Arrays.toString(count));
-		
-		int cntSize = count.length;
 
-		for (int i = 1; i < cntSize; i++) {
-			if(count[i-1] != count[i]) return 0;
-		}
-		return 1;
-	}
-
-	static String src = "wwoollfwolff";
+	static String src = "wowollff";
 }
 
 
