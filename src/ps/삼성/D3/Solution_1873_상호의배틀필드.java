9817861a087fbffe1c4a121b91629247ad22ac88
jaewon <jwsims@naver.com>
2021-08-05 02:13:26 +0900

    0805

--- src/ps/삼성/D3/Solution_1873_상호의배틀필드.java
+++ src/ps/삼성/D3/Solution_1873_상호의배틀필드.java
@@ -3,6 +3,7 @@ package ps.삼성.D3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -17,139 +18,161 @@ import java.util.StringTokenizer;
 
 
 public class Solution_1873_상호의배틀필드 {
+	static class Point{
+		int r,c;
+		char d;
+		public Point(int r, int c, char d) {
+			this.r = r;
+			this.c = c;
+			this.d = d;
+		}
+	}
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder output;
+	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
 	static int T, H, W, N;
+	static int nowR, nowC;
 	static char [][] map;
 	static char [] move;
-	static int[][] deltas = {{0,-1}, {-1,0}, {0,1}, {0,1}};
-	
+	static int[][] deltas = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+	static Point tank;
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		input = new BufferedReader(new StringReader(src));
 		T = Integer.parseInt(input.readLine());
 		for(int t=1; t<=T; t++) {
 			tokens = new StringTokenizer(input.readLine());
 			H = Integer.parseInt(tokens.nextToken());
 			W = Integer.parseInt(tokens.nextToken());
 			map = new char[H][W];
-			
-			for(int i=0; i<H; i++) {
+			for (int r = 0; r < H; r++) {
 				String line = input.readLine();
-				for(int j=0; j<W; j++) {
-					map[i][j] = line.charAt(j);
-				}
-			}
+				map[r] = line.toCharArray();
+			}//입력완료
+//			for(char[] c : map) {
+//				System.out.println(Arrays.toString(c));
+//			}//입력확인완료
+			
 			N = Integer.parseInt(input.readLine());
-			move= new char[N];
-			String line = input.readLine();
-			for (int n = 0; n < N; n++) {
-				move[n] = line.charAt(n);
-			}
-			//입력확인
-//			for (char[] row : map) {
-//				System.out.println(Arrays.toString(row));
-//			}
-			/////알고리즘 구현
+			move = input.readLine().toCharArray();
+			start(findTank());
 			
-			sol();
+			output.append("#").append(t).append(" ");
 			
-			for (char[] row : map) {
-				System.out.println(Arrays.toString(row));
+			for (int r = 0; r < H; r++) {
+				for (int c = 0; c < W; c++) {
+					output.append(map[r][c]);
+				}
+				output.append("\n");
 			}
-			//////
 		}
+		System.out.println(output);
+	}
+	
+	private static void start(Point tank) {
+		int moveCnt = move.length;
+		int dir = 0;
+		int r = tank.r;
+		int c = tank.c;
+		for(int m=0; m<moveCnt; m++) {
+			
+			char key = move[m];
+			
+			
+			if(key == 'S') {
+				if(map[r][c]=='^') {
+					dir = 0;
+				}else if(map[r][c] == 'v') {
+					dir = 1;
+				}else if(map[r][c] == '<') {
+					dir = 2;
+				}else if(map[r][c] == '>') {
+					dir = 3;
+				}
+				shot(dir,r,c);
+				
+			}else if(key == 'U') {
+				map[r][c] = '^';
+				if(!isIn(r-1,c))continue;
+				if(map[r-1][c] == '.') {
+					map[r-1][c] = '^';
+					map[r][c] = '.';
+					r= r-1;
+//					tank = new Point(r-1,c,'^');
+				}
+			}else if(key == 'R') {
+				map[r][c] = '>';
+				if(!isIn(r,c+1))continue;
+				if(map[r][c+1] == '.') {
+					map[r][c+1] = '>';
+					map[r][c] = '.';
+					c = c+1;
+//					tank = new Point(r,c+1,'>');
+				}
+			}else if(key == 'L') {
+				map[r][c] = '<';
+				if(!isIn(r,c-1))continue;
+				if(map[r][c-1] == '.') {
+					map[r][c-1] = '<';
+					map[r][c] = '.';
+					c = c-1;
+//					tank = new Point(r,c-1,'<');
+				}
+			}else if(key == 'D') {
+				map[r][c] = 'v';
+				if(!isIn(r+1,c))continue;
+				if(map[r+1][c] == '.') {
+					map[r+1][c] = 'v';
+					map[r][c] = '.';
+					r= r+1;
+//					tank = new Point(r+1,c,'v');
+				}
+			}
+		}
+	}
 
-	}
-	
-	static void sol() {
-		int posX=0;
-		int posY=0;
-		//먼저 탱크를 찾아
-		a:for (int r = 0; r < H; r++) {
-			for (int c = 0; c < W; c++) {
-				if(map[r][c] == '^' || map[r][c] == 'v'|| map[r][c] == '<'|| map[r][c] == '>') {
-					System.out.println("탱크 찾앗다!!");
-					posX = r;
-					posY = c;
-					System.out.println("반복?");
-					break a;
-				}
-			}
+	private static void shot(int dir, int r, int c) {
+		
+		int i = 1;
+		while(true) {			
 			
-		}
-		//System.out.println(posX + " "+ posY);
-		for (int n = 0; n < N; n++) {
-			action(move[n], posX, posY);
-			for (char[] row : map) {
-				System.out.println(row);
+			int nr = r + deltas[dir][0] * i;
+			int nc = c + deltas[dir][1] * i;
+			
+			if(!isIn(nr,nc)) return;
+			
+			if(map[nr][nc] == '#') return;
+			
+			if(map[nr][nc]=='*') {
+				map[nr][nc] = '.';
+				return;
 			}
-			System.out.println();
-		}
-	}
-	
-	static void action(char key, int r, int c) {
-		if(isIn(r-1,c) && key == 'U') {
-			if(map[r-1][c] == '.') {
-				System.out.println(map[r-1]);
-				map[r-1][c] = map[r][c];
-				map[r][c] = '.';
-				r--;
-			}
-		}
-		else if(isIn(r+1,c) && key == 'D'){
-			if(map[r+1][c] == '.') {
-				map[r+1][c] = map[r][c];
-				map[r][c] = '.';
-				r++;
-			}
-		}
-		else if(isIn(r,c-1) && key == 'L'){
-			if(map[r][c-1] == '.') {
-				map[r][c-1] = map[r][c];
-				map[r][c] = '.';
-				c--;
-			}
-		}
-		else if(isIn(r,c+1) && key == 'R'){
-			if(map[r][c+1] == '.') {
-				map[r][c+1] = map[r][c];
-				map[r][c] = '.';
-				c++;
-			}
-		}
-		else {	//key == 'S'
-			if(isIn(r-1,c) && map[r][c] == '^') {
-				for (int i = 1; i < H - r; i++) {
-					if(map[r-i][c] == '*') {
-						map[r-i][c] = '.';
-					}
-				}
-			}
-			else if(isIn(r+1,c) && map[r][c] == 'v') {
-				for (int i = 1; i < H - r; i++) {
-					if(map[r+i][c] == '*') {
-						map[r+i][c] = '.';
-					}
-				}
-			}
-			else if(isIn(r,c-1) && map[r][c] == '<') {
-				for (int i = 1; i < H - c; i++) {
-					if(map[r][c-i] == '*') {
-						map[r][c-i] = '.';
-					}
-				}
-			}
-			else if(isIn(r,c+1) && map[r][c] == '>') {
-				for (int i = 1; i < H - c; i++) {
-					if(map[r][c+i] == '*') {
-						map[r][c+i] = '.';
-					}
-				}
-			}
+			i+=1;
 		}
 	}
-	
+
+	private static Point findTank() {
+		tank = null;
+		for(int r = 0; r<H; r++) {
+			for(int c=0; c<W;c++) {
+				if(map[r][c] == '^' ||map[r][c] == 'v' ||map[r][c] == '<' ||map[r][c] == '>') {
+					tank = new Point(r,c, map[r][c]);
+					return tank;
+				}
+			}
+		}
+		return tank;
+	}
+
 	static boolean isIn(int r, int c) {
 		return 0<=r && r<H && 0<=c && c<W;
 	}
+	
+	static String src = "1\n" + 
+			"4 6\n" + 
+			"*.*..*\n" + 
+			"*.....\n" + 
+			"..-...\n" + 
+			"^.*#..\n" + 
+			"0\n" + 
+			"\n";
 }
