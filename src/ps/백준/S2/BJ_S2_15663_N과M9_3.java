8280164fb96a0c915daa3c713dbae50b241cd136
jaewon <jwsims@naver.com>
2021-08-11 00:38:53 +0900

    0811

--- src/ps/백준/S2/BJ_S2_15663_N과M9_3.java
+++ src/ps/백준/S2/BJ_S2_15663_N과M9_3.java
@@ -8,36 +8,74 @@ public class BJ_S2_15663_N과M9_3 {
 	static StringTokenizer tokens;
 	static StringBuilder output = new StringBuilder();
 	static int[] arr;
+	static Set<String> set = new HashSet<>();
+
 	public static void main(String[] args) throws IOException {
-		input= new BufferedReader(new StringReader(src));
+		input = new BufferedReader(new StringReader(src));
 		tokens = new StringTokenizer(input.readLine());
 		int N = Integer.parseInt(tokens.nextToken());
 		int M = Integer.parseInt(tokens.nextToken());
 		arr = new int[N];
 		tokens = new StringTokenizer(input.readLine());
-		for(int n=0; n<N; n++) {
+		for (int n = 0; n < N; n++) {
 			arr[n] = Integer.parseInt(tokens.nextToken());
 		}
 		Arrays.sort(arr);
-		
-		permutation(M, new int[M], new boolean[M]);
-	}
-	private static void permutation(int toChoose, int[] choosed, boolean[] visited) {
-		if(toChoose == 0) {
-			return;
-		}
-		for(int i =0 ;i<choosed.length;i++) {
-			choosed[choosed.length-toChoose] = arr[i];
-			visited[]
-		}
-	}
-	private static void combination(int toChoose, int[] choosed) {
-		
+
+		// permutation(M, new int[M], new boolean[N]);
+		np(M, new int[M], 0);
+
+		System.out.println(output);
+
 	}
 
-	private static void powerSet(int toChoose, int[] choosed) {
-	
+	private static void permutation(int toChoose, int[] choosed, boolean[] visited) {
+		if (toChoose == 0) {
+			StringBuilder sb = new StringBuilder();
+			for (int i : choosed) {
+				sb.append(i).append(" ");
+			}
+			String str = sb.toString();
+			if (set.contains(str))
+				return;
+			set.add(str);
+
+			output.append(str).append("\n");
+			return;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			if (!visited[i]) {
+				choosed[choosed.length - toChoose] = arr[i];
+				visited[i] = true;
+				permutation(toChoose - 1, choosed, visited);
+				visited[i] = false;
+			}
+		}
 	}
-	static String src = "8 8\r\n" + 
-			"1 2 3 4 5 6 7 8";
+
+	private static void np(int toChoose, int[] choosed, int flag) {
+		if (toChoose == 0) {
+			StringBuilder sb = new StringBuilder();
+			for (int i : choosed) {
+				sb.append(i).append(" ");
+			}
+			String str = sb.toString();
+			if (set.contains(str))
+				return;
+			set.add(str);
+
+			output.append(str).append("\n");
+			return;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			if ((flag & (1 << i)) > 0)
+				continue;
+
+			choosed[choosed.length - toChoose] = arr[i];
+			np(toChoose - 1, choosed, flag | (1 << i));
+
+		}
+	}
+
+	static String src = "4 2\r\n" + "9 7 9 1";
 }
