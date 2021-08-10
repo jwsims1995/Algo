906a54cee3cdcaca0b678c286f263c845750d2a4
jaewon <jwsims@naver.com>
2021-08-10 23:12:10 +0900

    0810

--- src/ps/백준/S2/BJ_S2_15663_N과M9_3.java
+++ src/ps/백준/S2/BJ_S2_15663_N과M9_3.java
@@ -7,29 +7,35 @@ public class BJ_S2_15663_N과M9_3 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer tokens;
 	static StringBuilder output = new StringBuilder();
-
+	static int[] arr;
 	public static void main(String[] args) throws IOException {
 		input= new BufferedReader(new StringReader(src));
 		tokens = new StringTokenizer(input.readLine());
 		int N = Integer.parseInt(tokens.nextToken());
 		int M = Integer.parseInt(tokens.nextToken());
-		int[] arr = new int[N];
+		arr = new int[N];
 		tokens = new StringTokenizer(input.readLine());
 		for(int n=0; n<N; n++) {
 			arr[n] = Integer.parseInt(tokens.nextToken());
 		}
 		Arrays.sort(arr);
 		
-		permutation();
+		permutation(M, new int[M], new boolean[M]);
 	}
-	private static void permutation(int toChoose, int[] choosed) {
-		
+	private static void permutation(int toChoose, int[] choosed, boolean[] visited) {
+		if(toChoose == 0) {
+			return;
+		}
+		for(int i =0 ;i<choosed.length;i++) {
+			choosed[choosed.length-toChoose] = arr[i];
+			visited[]
+		}
 	}
-	private static void permutation(int toChoose, int[] choosed) {
+	private static void combination(int toChoose, int[] choosed) {
 		
 	}
 
-	private static void permutation(int toChoose, int[] choosed) {
+	private static void powerSet(int toChoose, int[] choosed) {
 	
 	}
 	static String src = "8 8\r\n" + 
