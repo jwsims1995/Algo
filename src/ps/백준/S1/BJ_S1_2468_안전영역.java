3e090e4320d464b013048b72940122be486bb44f
jaewon <jwsims@naver.com>
2021-09-14 21:47:17 +0900

    0914

--- src/ps/백준/S1/BJ_S1_2468_안전영역.java
+++ src/ps/백준/S1/BJ_S1_2468_안전영역.java
@@ -4,11 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BJ_S1_2468_안전영역 {
@@ -17,41 +14,71 @@ public class BJ_S1_2468_안전영역 {
 	static StringTokenizer tokens;
 	static int[][] deltas = {{0,1},{0,-1},{1,0},{-1,0}};
 	static int[][] map;
+	static boolean[][] visited;
 	static int N;
+	static int top, ans;
 	public static void main(String[] args) throws IOException {
 		input = new BufferedReader(new StringReader(src)); 
 		N = Integer.parseInt(input.readLine());
-//		for(int r=0;r<N; r++) {
-//			tokens = new StringTokenizer(input.readLine());
-//			for(int c=0;c<N;c++) {
-//				map[r][c] = Integer.parseInt(tokens.nextToken());
-//			}
-//		}
-		 
-		Map<String,Integer> map = new HashMap<>();
-		map.put("hi", map.getOrDefault("hi", 0)+1);
-		map.put("hi", map.getOrDefault("hi", 0)+1);
-		String[] str = {"123", "123"};
-        Set<String> set = new HashSet<>();
-        for(String l : set)
-        
-		for(String c : map.keySet())
-			
-		System.out.println(map);
-        Stack<String> stack = new Stack<>();
-        String.val
-		for(int h=0; h<=N; h++) {
-			
+		map= new int[N][N];
+		for(int r=0;r<N; r++) {
+			tokens = new StringTokenizer(input.readLine());
+			for(int c=0;c<N;c++) {
+				map[r][c] = Integer.parseInt(tokens.nextToken());
+				top =  Math.max(top, map[r][c]);
+			}
 		}
-
-		
+		for(int h=0; h<=top;h++) { // 차오르는 높이
+			//map 복제
+			visited = new boolean[N][N];
+			//visited처리
+			for(int r=0; r<N; r++) {
+				for(int c=0; c<N; c++) {
+					if(map[r][c] <= h) {
+						visited[r][c] = true;
+					}
+				}
+			}
+			ans = Math.max(ans, findMax(h));
+		}
+		System.out.println(ans);
 		
 	}
 	
-	static String src = "5\n" + 
-			"6 8 2 6 2\n" + 
-			"3 2 3 4 6\n" + 
-			"6 7 3 3 2\n" + 
-			"7 2 5 3 6\n" + 
-			"8 9 5 2 7";
+	private static int findMax(int h) {
+		int cnt =0 ;
+		for(int r=0; r<N; r++) {
+			for(int c=0; c<N; c++) {
+				if(!visited[r][c]) {
+					dfs(r,c);
+					cnt++;
+				}
+			}
+		}
+		return cnt;
+	}
+
+	private static void dfs(int r, int c) {
+		visited[r][c] = true;
+		for(int d=0; d<deltas.length; d++) {
+			int nr = r + deltas[d][0];
+			int nc = c + deltas[d][1];
+			if(isIn(nr,nc) && !visited[nr][nc]) {
+				dfs(nr,nc);
+			}
+		}
+	}
+
+	private static boolean isIn(int nr, int nc) {
+		return nr>=0 && nr<N && nc>=0 && nc<N;
+	}
+
+	static String src = "7\n" + 
+			"9 9 9 9 9 9 9\n" + 
+			"9 2 1 2 1 2 9\n" + 
+			"9 1 8 7 8 1 9\n" + 
+			"9 2 7 9 7 2 9\n" + 
+			"9 1 8 7 8 1 9\n" + 
+			"9 2 1 2 1 2 9\n" + 
+			"9 9 9 9 9 9 9";
 }
