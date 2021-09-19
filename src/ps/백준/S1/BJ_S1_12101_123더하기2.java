5002412f6f1dde91ecca880ce707df0cd60598e8
jaewon <jwsims@naver.com>
2021-09-19 23:19:03 +0900

    0919

---
+++ src/ps/백준/S1/BJ_S1_12101_123더하기2.java
@@ -0,0 +1,21 @@
+package ps.백준.S1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+public class BJ_S1_12101_123더하기2 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer tokens;
+	
+	public static void main(String[] args) throws IOException {
+		input = new BufferedReader(new StringReader(src));
+		
+		
+	}
+	
+	static String src = "2 3 1\r\n";
+}
---
+++ src/ps/백준/S3/BJ_S1_9095_123더하기.java
@@ -0,0 +1,35 @@
+package ps.백준.S3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.StringTokenizer;
+
+public class BJ_S1_9095_123더하기 {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer tokens;
+	public static void main(String[] args) throws IOException {
+		input = new BufferedReader(new StringReader(src));
+		int N = Integer.parseInt(input.readLine());
+		int[] dp = new int[11];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		for(int i=4; i<11; i++) {
+			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+		}
+		for(int n=0; n<N; n++) {
+			int num = Integer.parseInt(input.readLine());
+			output.append(dp[num]).append("\n");
+		}
+		System.out.println(output);
+		
+	}
+	
+	static String src = "3\n" + 
+			"4\n" + 
+			"7\n" + 
+			"10";
+}
